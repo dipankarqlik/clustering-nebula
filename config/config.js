ae2fa585cfb70b0547333b3d78ba469ff6651882
Glitch (k-means-cluster) <none>
2021-08-26 16:31:55 +0000

    🚡🍢 Updated with Glitch

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  qlikWebIntegrationId: "<ID>",
+  tenantDomain: "incognito.us.qlikcloud.com",
+  appId: "5bc9cb45-82c6-4aea-bf98-f1b150ea5ceb",
+  issuer: "incognito.us.qlikcloud.com",
+  keyid: "<ID>"
+};
